211e74145907c5848a310a92222b69d51745c265
NaveenKumar <naveenxofficial@gmail.com>
2024-08-16 17:47:24 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -14,7 +14,7 @@
                 <a href="property_list_without_react.php?city=Hyderabad">PG in Hyderabad</a>
             </div>
         </div>
-        <div class="footer-copyright">© 2020 Copyright PG Life </div>
+        <div class="footer-copyright">© 2023 Copyright PG Life </div>
     </div>
 </div>
 
